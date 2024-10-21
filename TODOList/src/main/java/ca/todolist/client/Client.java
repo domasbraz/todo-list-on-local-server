2c1870959d59061a0c72f63396c8ff85695ff633
Domas Brazdeikis <114153020+domasbraz@users.noreply.github.com>
2024-10-21 23:42:25 +0100

    Add files via upload

---
+++ TODOList/src/main/java/ca/todolist/client/Client.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.todolist.client;
+
+import java.io.*;
+import java.net.*;
+
+public class Client
+{
+
+    private static InetAddress host;
+    private static final int PORT = 1234;
+    private static boolean connected;
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            host = InetAddress.getLocalHost();
+        }
+        catch (UnknownHostException e)
+        {
+            System.out.println("Host ID not found!");
+            System.exit(1);
+        }
+        run();
+    }
+
+    private static void run()
+    {
+        Socket link = null;
+        try
+        {
+
+            connected = true;
+            
+            do
+            {
+                link = new Socket(host, PORT);
+                BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+                PrintWriter out = new PrintWriter(link.getOutputStream(), true);
+
+                BufferedReader userEntry = new BufferedReader(new InputStreamReader(System.in));
+                String message = null;
+                String response = null;
+
+                System.out.println("Enter message to be sent to server: ");
+                message = userEntry.readLine();
+                out.println(message);
+                response = in.readLine();
+                if (response.equals("TERMINATE"))
+                {
+                    connected = false;
+                }
+                System.out.println("\nSERVER RESPONSE> " + response);
+                while ((response = in.readLine()) != null)
+                {
+                    System.out.println(response);
+                }
+            }
+            while (connected);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                System.out.println("\n* Closing connection... *");
+                link.close();
+            }
+            catch (IOException e)
+            {
+                System.out.println("Unable to disconnect/close!");
+                System.exit(1);
+            }
+        }
+    }
+    
+    
+}
